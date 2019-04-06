207dd8e9206181d70ead46873ee90adfdfb6a96a
MatheusLealAquino <matheusaquino@id.uff.br>
2019-04-05 21:17:02 -0300

    first commit

---
+++ Client/src/plugins/axios.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost/studFreemium'
+})
+
+//axiosInstance.defaults.headers.common['Authorization'] = 'qq'
+
+export default ({ Vue }) => {
+  Vue.prototype.$axios = axiosInstance
+}
